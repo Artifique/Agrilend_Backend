e190a34dc652be0aaeac72a4c45809aa41404915
Artifique <fombadaouda72@gmail.com>
2025-09-21 14:17:29 +0000

    initial commit

---
+++ src/main/java/com/example/agrilend_backend/AgrilendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.agrilend_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrilendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrilendBackendApplication.class, args);
+	}
+
+}
